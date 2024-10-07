a1c7214744eacb5e95793883e442bc248cb4be98
Omkar Patil <git21cs072-t@students.git.edu>
2024-10-07 10:03:34 +0530

    Built A responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+
 import './App.css';
 
 function App() {
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+      <Grid
+        templateAreas={{
+          base: `'nav nav' 'main main'`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area='nav' bg='coral'>
+          Nav
+        </GridItem>
+        <Show above='lg'>
+          <GridItem area='aside' bg='gold'>
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area='main' bg='dodgerblue'>
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
